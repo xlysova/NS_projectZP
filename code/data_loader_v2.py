006002e3403dd30591cb0621b1aa4381f484430a
Zuzana Lysová <xlysova@mendelu.cz>
2024-01-19 13:00:25 +0100

    new dataset

--- code/data_loader_v2.py
+++ code/data_loader_v2.py
@@ -1,6 +1,6 @@
-#%%
+
+#%% 
 import pandas as pd
-import ast
 import nltk
 from nltk.corpus import stopwords
 
@@ -17,19 +17,18 @@ class DataLoader:
     def load_data(self):
         self.movies_df = pd.read_csv(self.file_path)
         self.movies_df['overview'] = self.movies_df['overview'].fillna('')
-        self.movies_df['genres'] = self.movies_df['genres'].apply(ast.literal_eval)
+        self.movies_df['genres'] = self.movies_df['genre'].str.split(', ')  # Changed line
         self.movies_df['sid'] = range(len(self.movies_df))
 
     def process_data(self):
-        exploded_genres = self.movies_df.explode('genres')
-        exploded_genres['genre'] = exploded_genres['genres'].apply(lambda x: x['name'] if pd.notna(x) else None)
-        exploded_genres.dropna(subset=['genre'], inplace=True)
-        self.final_df = exploded_genres[['sid', 'title', 'genre', 'overview']]
+        exploded_genres = self.movies_df.explode('genres')  # Changed line
+        exploded_genres.dropna(subset=['genres'], inplace=True)  # Changed line
+        self.final_df = exploded_genres[['sid', 'names', 'genres', 'overview']]  # Changed line
         self.final_df.drop_duplicates(inplace=True)
 
-        unique_genres = self.final_df['genre'].unique()
+        unique_genres = self.final_df['genres'].unique()  # Changed line
         genre_to_id = {genre: idx + 1 for idx, genre in enumerate(unique_genres)}
-        self.final_df['genre_id'] = self.final_df['genre'].map(genre_to_id)
+        self.final_df['genre_id'] = self.final_df['genres'].map(genre_to_id)  # Changed line
 
         self.final_df['overview'] = self.final_df['overview'].apply(self.remove_stop_words)
 
@@ -38,18 +37,18 @@ class DataLoader:
         overview_minus_sw = []
         stop_words = stopwords.words('english')
         overview = overview.lower().split()
-        final_overview = [overview_minus_sw.append(word) for word in overview if word not in stop_words]            
+        final_overview = [overview_minus_sw.append(word) for word in overview if word not in stop_words]
         final_overview = ' '.join(overview_minus_sw)
         return final_overview
 
     def analyze_genre_counts(self):
-        genre_counts = self.final_df['genre'].value_counts()
+        genre_counts = self.final_df['genres'].value_counts()  # Changed line
         self.genre_counts_df = genre_counts.reset_index()
         self.genre_counts_df.columns = ['Genre', 'Count']
 
     def analyze_action_genre(self):
         action_df = self.final_df.copy()
-        action_df['is_action'] = action_df['genre_id'] == 1
+        action_df['is_action'] = action_df['genres'] == 'Action'  # Changed line
         action_df = action_df.groupby('sid', as_index=False).agg({'overview': 'first', 'sid': 'first', 'is_action': 'any'})
         action_df['is_action'] = action_df['is_action'].astype(int)
         self.action_df = action_df
@@ -67,7 +66,7 @@ class DataLoader:
 
 #%%
 # Usage
-data_loader = DataLoader('data/movies.csv')
+data_loader = DataLoader('data/imdb_movies.csv')
 data_loader.load_data()
 data_loader.process_data()
 data_loader.analyze_genre_counts()
@@ -78,4 +77,5 @@ genre_counts_df = data_loader.get_genre_counts_df()
 action_df = data_loader.get_action_df()
 print(action_df)
 # Now you can use final_df, genre_counts_df, and action_df as needed.
+
 # %%
