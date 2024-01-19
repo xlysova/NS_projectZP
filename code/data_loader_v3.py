73f1d06626a3cabc105c77a99ca3445eab08c89c
Zuzana Lysová <xlysova@mendelu.cz>
2024-01-19 14:14:25 +0100

    v3 - basics

---
+++ code/data_loader_v3.py
@@ -0,0 +1,83 @@
+
+#%% 
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import re
+
+class DataLoader3:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.movies_df = None
+        self.final_df = None
+        self.genre_counts_df = None
+        self.action_df = None
+
+        nltk.download('stopwords')
+
+    def load_data(self):
+        self.movies_df = pd.read_csv(self.file_path)
+        self.movies_df['overview'] = self.movies_df.get('overview', '').fillna('')
+        # Use a regular expression to split the genres on commas and any kind of space
+        self.movies_df['genre'] = self.movies_df['genre'].apply(lambda x: re.split(r',\s*', x) if isinstance(x, str) else [])
+        self.movies_df['sid'] = range(len(self.movies_df))
+
+    def process_data(self):
+        # Explode the genres column to create separate rows for each genre
+        exploded_genres = self.movies_df.explode('genre')
+        exploded_genres.dropna(subset=['genre'], inplace=True)
+        self.final_df = exploded_genres[['names', 'sid', 'overview', 'genre']]
+        self.final_df.drop_duplicates(inplace=True)
+
+        unique_genres = self.final_df['genre'].unique()  # Changed line
+        genre_to_id = {genre: idx + 1 for idx, genre in enumerate(unique_genres)}
+        self.final_df['genre_id'] = self.final_df['genre'].map(genre_to_id)  # Changed line
+
+        self.final_df['overview'] = self.final_df['overview'].apply(self.remove_stop_words)
+
+    @staticmethod
+    def remove_stop_words(overview):
+        overview_minus_sw = []
+        stop_words = stopwords.words('english')
+        overview = overview.lower().split()
+        final_overview = [overview_minus_sw.append(word) for word in overview if word not in stop_words]
+        final_overview = ' '.join(overview_minus_sw)
+        return final_overview
+
+    def analyze_genre_counts(self):
+        genre_counts = self.final_df['genre'].value_counts()  # Changed line
+        self.genre_counts_df = genre_counts.reset_index()
+        self.genre_counts_df.columns = ['Genre', 'Count']
+
+    def analyze_action_genre(self):
+        action_df = self.final_df.copy()
+        action_df = action_df.groupby('sid', as_index=False).agg({'sid': 'first', 'names': 'first', 'genre_id': 'first', 'genre': 'first', 'overview': 'first'})
+        self.action_df = action_df
+        self.action_df['overview'] = self.action_df['overview'].apply(self.remove_stop_words)
+
+
+    def get_final_df(self):
+        return self.final_df
+
+    def get_genre_counts_df(self):
+        return self.genre_counts_df
+
+    def get_action_df(self):
+        return self.action_df
+
+#%%
+# 
+# Usage
+data_loader = DataLoader3('data/imdb_movies.csv')
+data_loader.load_data()
+data_loader.process_data()
+data_loader.analyze_genre_counts()
+data_loader.analyze_action_genre()
+
+final_df = data_loader.get_final_df()
+genre_counts_df = data_loader.get_genre_counts_df()
+action_df = data_loader.get_action_df()
+print(action_df)
+# Now you can use final_df, genre_counts_df, and action_df as needed.
+
+# %%
---
+++ code/model_v3.py
@@ -0,0 +1,121 @@
+#%%
+from data_loader_v2 import DataLoader2
+from sklearn.model_selection import train_test_split 
+from numpy import array
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+data_loader = DataLoader2('data/imdb_movies.csv')
+data_loader.load_data()
+data_loader.process_data()
+data_loader.analyze_genre_counts()
+data_loader.analyze_action_genre()
+
+final_df = data_loader.get_final_df()
+genre_counts_df = data_loader.get_genre_counts_df()
+action_df = data_loader.get_action_df()
+print(action_df)
+
+# %%
+X = action_df['overview']
+y = array(action_df['is_action'])
+
+X_train, X_test , y_train, y_test = train_test_split(X, y , test_size = 0.30)
+
+# %%
+oov_token = "<OOV>"
+tokenizer = Tokenizer(oov_token=oov_token)
+tokenizer.fit_on_texts(X_train)
+
+# %%
+word_index = tokenizer.word_index
+
+# %%
+X_train_sequences = tokenizer.texts_to_sequences(X_train)
+X_test_sequences = tokenizer.texts_to_sequences(X_test)
+
+# %%
+padding_type = "post"
+trunction_type="post"
+
+# Find the length of each sequence
+sequence_lengths_train = [len(seq) for seq in X_train_sequences]
+sequence_lengths_test = [len(seq) for seq in X_test_sequences]
+sequence_lengths = sequence_lengths_train + sequence_lengths_test
+# Determine the maximum length
+max_length = max(sequence_lengths)
+# max_length = 512
+
+# %%
+X_train_padded = pad_sequences(X_train_sequences,maxlen=max_length, padding=padding_type, truncating=trunction_type)
+
+# %%
+X_test_padded = pad_sequences(X_test_sequences,maxlen=max_length, padding=padding_type,
+                              truncating=trunction_type)
+
+# %%
+tf.random.set_seed(0)
+
+# %%
+training_data = tf.data.Dataset.from_tensor_slices((X_train_padded, y_train))
+validation_data = tf.data.Dataset.from_tensor_slices((X_test_padded, y_test))
+
+# %%
+batch_size = 64
+training_data = training_data.batch(batch_size)
+validation_data = validation_data.batch(batch_size)
+
+#%%
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+
+# %%
+epochs = 5
+
+#%%
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Embedding, LSTM, Bidirectional
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Embedding
+
+vocab_size = len(tokenizer.word_index) + 1
+# vocab_size = 5000
+model = Sequential([
+    Embedding(vocab_size, 64, input_length=max_length),
+    Bidirectional(LSTM(32, return_sequences=True)),
+    Bidirectional(LSTM(32,)),
+    Dense(5, activation='relu'),
+    Dense(1, activation='sigmoid')])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(training_data, epochs=epochs, verbose=1,validation_data = validation_data, callbacks = [callback])
+
+# %%
+loss, accuracy = model.evaluate(training_data)
+print('Training Accuracy is {}'.format(accuracy*100))
+
+#%%
+loss, accuracy = model.evaluate(validation_data)
+print('Testing Accuracy is {} '.format(accuracy*100))
+
+# %%
+import pandas as pd
+hist = pd.DataFrame(history.history)
+
+# %%
+import matplotlib.pyplot as plt
+plt.figure(figsize=(7,6))
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+hist['loss'].plot(label='Training Loss')
+hist['val_loss'].plot(label='Validation Loss')
+plt.legend(loc='center')
+
+# %%
+plt.figure(figsize=(7,6))
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+hist['acc'].plot(label='Training Accuracy')
+hist['val_acc'].plot(label='Validation Accuracy')
+plt.legend(loc='center')
+# %%
